cef27e9ef655997422eb5203a042516d618db8d2
James Sumners <james.sumners@gmail.com>
2016-01-21 15:43:52 -0500

    Update userExists tests

--- test/mockServer/search.js
+++ test/mockServer/search.js
@@ -3,9 +3,11 @@
 const ldap = require('ldapjs');
 
 const findUserFilter = '(&(objectcategory=User)(|(samaccountname=username@domain.com)(userprincipalname=username@domain.com)))';
+const userExistsFilter1 = '(&(objectcategory=User)(|(samaccountname=username)(userprincipalname=username)))';
 
 module.exports = function search(server, settings) {
   const findUser = settings.findUser;
+  const userExists = settings.userExists;
   const baseDN = 'dc=domain,dc=com';
 
   function findUserSearch(req, res, next) {
@@ -38,8 +40,14 @@ module.exports = function search(server, settings) {
   server.search(
     baseDN,
     function searchHandler(req, res, next) {
-      if (req.filter.toString() === findUserFilter) {
-        return findUserSearch(req, res, next);
+      switch (req.filter.toString()) {
+        case findUserFilter:
+        case userExistsFilter1:
+          return findUserSearch(req, res, next);
+          break;
+        default:
+          res.end();
+          break;
       }
     }
   );
--- test/userexists.js
+++ test/userexists.js
@@ -1,44 +1,54 @@
-var assert = require('assert');
-var ActiveDirectory = require('../index');
-var config = require('./config');
+'use strict';
 
-describe('ActiveDirectory', function() {
-  var ad;
-  var settings = require('./settings').userExists;
+const expect = require('chai').expect;
+const ActiveDirectory = require('../index');
+const config = require('./config');
 
-  before(function() {
-   ad = new ActiveDirectory(config);
+let server = require('./mockServer');
+
+describe('userExists Method', function() {
+  let ad;
+  const settings = require('./settings').userExists;
+
+  before(function(done) {
+    server(function(s) {
+      ad = new ActiveDirectory(config);
+      server = s;
+      done();
+    });
   });
 
-  describe('#userExists()', function() {
-    it('should return true if the username (sAMAccountName) exists', function(done) {
-      ad.userExists(settings.username.sAMAccountName, function(err, exists) {
-        if (err) return(done(err));
-        assert(exists);
-        done();
-      });
-    });
-    it('should return true if the username (userPrincipalName) exists', function(done) {
-      ad.userExists(settings.username.userPrincipalName, function(err, exists) {
-        if (err) return(done(err));
-        assert(exists);
-        done();
-      });
-    });
-    it('should return true if the username (distinguishedName) exists', function(done) {
-      ad.userExists(settings.username.sAMAccountName, function(err, exists) {
-        if (err) return(done(err));
-        assert(exists);
-        done();
-      });
-    });
-    it('should return false if the username doesn\'t exist', function(done) {
-      ad.userExists('!!!NON-EXISTENT USER!!!', function(err, exists) {
-        if (err) return(done(err));
-        assert(! exists);
-        done();
-      });
+  it('should return true if the username (sAMAccountName) exists', function(done) {
+    ad.userExists(settings.username.sAMAccountName, function(err, exists) {
+      expect(err).to.be.null;
+      expect(exists).to.be.true;
+      done();
     });
   });
+
+  it('should return true if the username (userPrincipalName) exists', function(done) {
+    ad.userExists(settings.username.userPrincipalName, function(err, exists) {
+      expect(err).to.be.null;
+      expect(exists).to.be.true;
+      done();
+    });
+  });
+
+  it('should return true if the username (distinguishedName) exists', function(done) {
+    ad.userExists(settings.username.sAMAccountName, function(err, exists) {
+      expect(err).to.be.null;
+      expect(exists).to.be.true;
+      done();
+    });
+  });
+
+  it('should return false if the username doesn\'t exist', function(done) {
+    ad.userExists('!!!NON-EXISTENT USER!!!', function(err, exists) {
+      expect(err).to.be.null;
+      expect(exists).to.be.false;
+      done();
+    });
+  });
+
 });
 
