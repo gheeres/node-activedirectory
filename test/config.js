2378f9ad5991622fe8ed601990d256faaacd6d03
James Sumners <james@sumners.email>
2020-01-12 10:26:52 -0500

    Refacor ctor tests

--- test/config.js
+++ test/config.js
@@ -2,13 +2,14 @@ const pino = require('pino')
 const pretty = pino.pretty()
 pretty.pipe(process.stdout)
 
-module.exports = function config(port = 1389) {
+module.exports = function config (port = 1389) {
   return {
     url: `ldap://127.0.0.1:${port}`,
     baseDN: 'dc=domain,dc=com',
     username: 'auth@domain.com',
     // username: 'CN=Authenticator,OU=Special Uesrs,DC=domain,DC=com',
     password: 'password',
+    reconnect: false,
     logging: pino({level: 'error'}, pretty)
   }
 }
